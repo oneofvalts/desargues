896315444331b97e67fd263efbff9ce062f70d20
Abdullah Uyu <abdullahuyu@gmail.com>
2024-04-02 20:22:49 +0300

    Understand instances can be supplied to context as parameters

--- lab/rank.lean
+++ lab/rank.lean
@@ -13,11 +13,6 @@ have pq_card : (card {p, q}) = 2 := by aesop
 rw [<- pq_card]
 apply finrank_span_finset_le_card {p, q}
 
-theorem pq_finite
-(p q : V) :
-Set.Finite {p, q} := by
-aesop
-
 theorem pq_subspace_finite
 (p q : V) :
 Module.Finite K (span K {p, q}) := by
@@ -26,12 +21,13 @@ aesop -- proves Set.Finite {p,q}
 
 theorem abp_rank_le_span_rank
 (a b p q : V)
+(finite_submodule_instance := pq_subspace_finite (K := K) (V := V) p q)
 (pq_neq : p ≠ q)
 (abp_indep : LinearIndependent K ![a, b, p])
 (a_span_pq : a ∈ (span K {p, q}))
 (b_span_pq : b ∈ (span K {p, q})) :
 (card ({a, b, p} : Finset (span K {p, q}))) ≤ finrank K (span K {p, q}) := by
-apply finset_card_le_finrank (R := K) (M := M) {a, b, p}
+apply finset_card_le_finrank (R := K) (M := (span K {p, q})) {a, b, p}
 -- overloaded, errors
 --   failed to synthesize instance
 --     Singleton V (Finset ↥(span K ↑{p, q}))
