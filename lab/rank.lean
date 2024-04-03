f7f60a09122cfc35653561d0ea7d743cebef4647
Abdullah Uyu <abdullahuyu@gmail.com>
2024-04-04 00:01:45 +0300

    Understand Richard Copley's explanation on explicit parameters, learn indentation

--- lab/rank.lean
+++ lab/rank.lean
@@ -1,4 +1,3 @@
-import Mathlib.RingTheory.Finiteness
 import Mathlib.LinearAlgebra.FiniteDimensional
 
 open Finset Submodule FiniteDimensional LinearIndependent
@@ -6,30 +5,27 @@ open Finset Submodule FiniteDimensional LinearIndependent
 variable [DivisionRing K] [AddCommGroup V] [Module K V] [DecidableEq V]
 
 theorem pq_span_dim_ineq
-(p q : V)
-(pq_neq : p ≠ q) :
-Set.finrank (R := K) (toSet {p, q}) ≤ 2 := by
-have pq_card : (card {p, q}) = 2 := by aesop
-rw [<- pq_card]
-apply finrank_span_finset_le_card {p, q}
+    (p q : V)
+    (pq_neq : p ≠ q) :
+    Set.finrank (R := K) (toSet {p, q}) ≤ 2 := by
+  have pq_card : (card {p, q}) = 2 := by aesop
+  rw [<- pq_card]
+  apply finrank_span_finset_le_card {p, q}
 
-theorem pq_subspace_finite
-(p q : V) :
-Module.Finite K (span K {p, q}) := by
-apply Module.Finite.span_of_finite
-aesop -- proves Set.Finite {p,q}
+theorem pq_subspace_finite (p q : V) :
+    Module.Finite K (Submodule.span K {p, q}) := by
+  exact Module.Finite.span_of_finite K (Set.toFinite {p, q})
 
-theorem abp_rank_le_span_rank
-(a b p q : V)
-(finite_submodule_instance := pq_subspace_finite (K := K) (V := V) p q)
-(pq_neq : p ≠ q)
-(abp_indep : LinearIndependent K ![a, b, p])
-(a_span_pq : a ∈ (span K {p, q}))
-(b_span_pq : b ∈ (span K {p, q})) :
-(card ({a, b, p} : Finset (span K {p, q}))) ≤ finrank K (span K {p, q}) := by
-apply finset_card_le_finrank (R := K) (M := (span K {p, q})) {a, b, p}
--- overloaded, errors
---   failed to synthesize instance
---     Singleton V (Finset ↥(span K ↑{p, q}))
---
---   28:2 'a' is not a field of structure 'Finset'
+theorem abp_card_le_span_rank
+    (a b p q : V)
+    (finite_submodule_instance := pq_subspace_finite (K := K) (V := V) p q)
+    (pq_neq : p ≠ q)
+    (abp_indep : LinearIndependent K ![a, b, p])
+    (a_span_pq : a ∈ (span K {p, q}))
+    (b_span_pq : b ∈ (span K {p, q})) :
+    (card {a, b, p}) ≤ finrank K (span K {p, q}) := by
+  apply finset_card_le_finrank (R := K) (M := (span K {p, q})) {a, b, p}
+  -- tactic 'apply' failed, failed to unify
+  --   ?m.9988.card ≤ finrank K ↥(span K {p, q})
+  -- with
+  --   {a, b, p}.card ≤ finrank K ↥(span K {p, q})
