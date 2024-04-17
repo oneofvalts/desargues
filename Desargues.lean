e1dd6adc55aa90c16d0353a388e037207d3ad663
Abdullah Uyu <abdullahuyu@gmail.com>
2024-04-17 17:15:52 +0300

    Finish the proof of P_3

--- Desargues.lean
+++ Desargues.lean
@@ -1,5 +1,7 @@
 import Mathlib.Tactic
 
+open Set
+
 namespace Desargues
 
 variable [DecidableEq G]
@@ -117,11 +119,144 @@ theorem p_2
       apply rel_sym_bca a b a
       apply PG.l1 a b
 
--- theorem p_3
---   {PG : ProjectiveGeometry G} :
---     ∀ a b c d p, a ∈ star (PG := PG) b p → p ∈ star (PG := PG) c d → a ≠ c →
---       star (PG := PG) a c ∩ star (PG := PG) b d ≠ ∅ := by
---   intro a b c d p
---   intro a_in_bp p_in_cd ac_neq inter_empty
+lemma star_imp_ell
+  {PG : ProjectiveGeometry G}
+  (x y z : G)
+  (x_in_yz : x ∈ star (PG := PG) y z) :
+    PG.ell x y z := by
+  obtain rfl | yz_neq := eq_or_ne y z
+  · apply rel_sym_bca y x y
+    apply PG.l1 y x
+  · unfold star at x_in_yz
+    split at x_in_yz
+    case inr.inl eq => apply yz_neq at eq; contradiction
+    case inr.inr _ =>
+      simp at x_in_yz
+      apply rel_sym_cab y z x
+      exact x_in_yz
+
+lemma abc_ncol_imp_neq
+  {PG : ProjectiveGeometry G}
+  (a b c : G)
+  (abc_ncol : ¬ PG.ell a b c):
+    a ≠ b ∧ a ≠ c ∧ b ≠ c := by
+  constructor
+  case left =>
+    intro ab_eq
+    rw [ab_eq] at abc_ncol
+    apply abc_ncol
+    apply rel_sym_cab b c b
+    apply PG.l1 b c
+  case right =>
+    constructor
+    case left =>
+      intro ac_eq
+      rw [ac_eq] at abc_ncol
+      apply abc_ncol
+      apply PG.l1 c b
+    case right =>
+      intro bc_eq
+      rw [bc_eq] at abc_ncol
+      apply abc_ncol
+      apply rel_sym_bca c a c
+      apply PG.l1 c a
+
+theorem p_3
+  {PG : ProjectiveGeometry G} :
+    ∀ a b c d p, a ∈ star (PG := PG) b p → p ∈ star (PG := PG) c d → a ≠ c →
+      star (PG := PG) a c ∩ star (PG := PG) b d ≠ ∅ := by
+  intro a b c d p
+  intro a_in_bp p_in_cd ac_neq inter_empty
+  by_cases abc_col : PG.ell a b c
+  · have b_in_inter :
+        b ∈ star (PG := PG) a c ∩ star (PG := PG) b d := by
+      rw [inter_def]
+      simp
+      constructor
+      · unfold star
+        split
+        case left.inl eq =>
+          apply ac_neq at eq
+          contradiction
+        case left.inr neq =>
+          simp
+          apply rel_sym_acb a b c
+          exact abc_col
+      · unfold star
+        split
+        case right.inl _ =>
+          simp
+        case right.inr _ =>
+          simp
+          apply PG.l1 b d
+    rw [inter_empty] at b_in_inter
+    exact b_in_inter
+  · have abp_col :
+        PG.ell a b p := by
+      apply star_imp_ell a b p a_in_bp
+    have pcd_col :
+        PG.ell p c d := by
+      apply star_imp_ell p c d p_in_cd
+    have abc_neq :
+        a ≠ b ∧ a ≠ c ∧ b ≠ c := by
+      apply abc_ncol_imp_neq a b c
+      exact abc_col
+    have ab_neq :
+        a ≠ b := by
+      cases abc_neq with
+      | intro left _ => exact left
+    have bp_neq :
+        b ≠ p := by
+      intro bp_eq
+      rw [bp_eq] at a_in_bp
+      have ab_eq :
+          a = b := by
+        unfold star at a_in_bp
+        simp at a_in_bp
+        rw [<- bp_eq] at a_in_bp
+        exact a_in_bp
+      contradiction
+    have bd_neq :
+        b ≠ d := by
+      intro bd_eq
+      rw [<- bd_eq] at pcd_col
+      apply abc_col -- ℓ(a, c, b) follows from L₂, now.
+      apply rel_sym_acb a c b
+      apply PG.l2 a c b p
+      · exact abp_col
+      · apply rel_sym_bca p c b
+        exact pcd_col
+      · exact bp_neq
+    have q_ex :
+        exists q, PG.ell q a c ∧ PG.ell q b d := by
+      apply PG.l3 a b c d p
+      · apply rel_sym_cab a b p
+        exact abp_col
+      · exact pcd_col
+    match q_ex with
+    | ⟨q, qac_col, qbd_col⟩ =>
+      have q_in_inter :
+          q ∈ star (PG := PG) a c ∩ star (PG := PG) b d := by
+        rw [inter_def]
+        simp
+        constructor
+        · unfold star
+          split
+          case left.inl ac_eq =>
+            contradiction
+          case left.inr _ =>
+            simp
+            apply rel_sym_bca q a c
+            exact qac_col
+        · unfold star
+          split
+          case right.inl bd_eq =>
+            contradiction
+          case right.inr _ =>
+            simp
+            apply rel_sym_bca q b d
+            exact qbd_col
+      rw [inter_empty] at q_in_inter
+      exact q_in_inter
 
 end Desargues
